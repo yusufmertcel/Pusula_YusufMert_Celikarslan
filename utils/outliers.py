e2a794fcae4bbe922547cf859c155dce7ae14b75
Yusuf Mert Çelikarslan <yusufmertcelikarslan@gmail.com>
2024-09-20 18:38:29 +0300

    Outlier functions placed into outliers.py

---
+++ utils/outliers.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def outlier_detect(df):
+    Q1 = df.quantile(0.25)
+    Q3 = df.quantile(0.75)
+    IQR = Q3 - Q1
+    lower = Q1 - 1*IQR
+    upper = Q3 + 1*IQR
+    print('Before data preprocess:')
+    print('Skewness:\n', df.skew())
+    print(df.describe())
+    print('Median:\n', df.median())
+    print('IQR:\n', IQR)
+    print("Lower - Uppeer bound", lower,'-',upper)
